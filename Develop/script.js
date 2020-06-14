fa7bf8908f62f47333aa81dc91775f91347c3755
unknown <pv95335@gmail.com>
2020-06-14 14:33:30 -0700

    testing code no solutions

--- Develop/script.js
+++ Develop/script.js
@@ -1,83 +1,119 @@
 // Assignment code here
+
+
+//System Functions Start
+//Will start the generate password function
 var generatePassword = function() {
-  //if (generatePassword == null || generatePassword =="") {
-    //window.alert("Goodbye");
-  //} else {
- criteria()
-  }
-//}
+  criteria()
 
-var generateBtn = document.querySelector("#generate");
+   }
+ 
+ 
+ var generateBtn = document.querySelector("#generate");
+ 
+ function writePassword() {
 
-function writePassword() {
+  window.alert(generatePassword());
+
+
+
+   //Variable for password will = generatePassword
+   var password = generatePassword();
+
+   //Document.querySelector and value is id=password in html
+   var passwordText = document.querySelector("#password");
+   passwordText.value = password;
+ 
+ 
+ }
+
+//System Functions End
+
+
+//Functions for variables Start
+
+//function generate() {
+  //let password = "";
+
+  //let length = +document.getElementById("length").value;
+
+ // let lowerCase = document.getElementById("lowercase").confirm;
+ // let upperCase = document.getElementById("uppercase").confirm;
+ // let numberRandom = document.getElementById("numberRandom").confirm;
+ // let symbolRandom = document.getElementById("symbolRandom").confirm;
   
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+ // if (lowerCase + upperCase + symbolRandom + numberRandom <= 0)
+ // return;
 
+//  for (let i=0; i < length; i++) {
 
-}
-
-generateBtn.addEventListener("click", writePassword);
-
-
-
-//Generating
-function criteria() {
-  var passwordLength = window.prompt("How long do you want it? Type a number between 8 - 128.")
-    switch (passwordLength) {
-
-    }
-     window.confirm("Do you want lower case letters?")
-      if (confirm(lowerLetter()));
-    
-
-    window.confirm("Do you want uppper case letters?")
-      if (confirm(upperLetter()));
-    
-
-    window.confirm("Do you want numbers?")
-      if (confirm(numberRandom()));
-
-
-    window.confirm("Do you want symbols?")
-      if (confirm(symbolRandom()));
-    
-}
+  //  const r=generater(0,3);
+  //  if (lowerCase && r === 0){
+  //    password += generateRandomLowerCase();
+  //  } else if (upperCase && r === 1){
+   //   password += generateRandomUpperCase();
+   // } else if (symbolRandom && r === 2){
+  //    password += generateRandomSymbol();
+   // } else if (number && r === 3){
+   //   password += generater(0,9);
+   // } else {
+   //   i--;
+   // }
+  //}
+  //document.getElementById("result").textContent = password;
+//}
+//generate();
 
 function lowerLetter(){
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
   
-  
+  //Random generation for uppercase letter
   function upperLetter() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 
   }
-  
+  //Random generation for number between 0-9
   function numberRandom() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48); 
   }
-  
+  //Random generation for symbols !@#$%^&*
   function symbolRandom(){
     const symbols = '!@#$%^&*';
     return symbols[Math.floor(Math.random() * symbols.length)];
   }
 
-  const randomGenerate = {
-    lower: lowerLetter,
-    upper: upperLetter,
-    number: numberRandom,
-    symbol: symbolRandom
-  };
+//Functions for variables End
 
-const resultEl = document.getElementById('resultEl');
-const lengthEl = document.getElementById('lengthEl');
-const upperCaseEl = document.getElementById('upperCaseEl');
-const lowerCaseEl = document.getElementById('lowerCaseEl');
-const numbersEl = document.getElementById('numbersEl');
-const symbolsEl = document.getElementById('symbolsEl');
-const generateEl = document.getElementById('generateEl');
+//User Input Start
+generateBtn.addEventListener("click", writePassword);
 
+  //Will ask the users for the criteria they wish to use in confirms and prompt
+function criteria() {
+  var passwordLength = window.prompt("How long do you want it? Type a number between 8 - 24.")
+
+  var lowerCaseLetter = window.confirm("Do you want lower case letters?")
+      if (confirm(lowerLetter()));
+    
+  var upperCaseLetter = window.confirm("Do you want uppper case letters?")
+      if (confirm(upperLetter()));
+    
+  var randomNumbers = window.confirm("Do you want numbers?")
+      if (confirm(numberRandom()));
+
+  var randomSymbols = window.confirm("Do you want symbols?")
+      if (confirm(symbolRandom()));
+    
+}
+
+//User Input End
+
+
+//Random Generation Start
+
+  //Random generation for lowercase letter
+
+
+//Random Generation End
 
 
 //STARTING CODE FOR ASSIGNMENT
